600dedcb4f2b0b7ea0344b436ac702c85d6232bc
Navid Bonyadi <navidprinceofpersia@gmail.com>
2025-04-29 21:56:34 +0330

    create login

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,60 +1,40 @@
-import { defineNuxtConfig } from 'nuxt/config'
-
 export default defineNuxtConfig({
   app: {
     head: {
-      // Meta tags and other head elements
+      htmlAttrs: {
+        dir: 'rtl',
+        lang: 'fa'
+      },
       link: [
         // Favicon
         { rel: 'icon', type: 'image/x-icon', href: '/assets/img/favicon/favicon.ico' },
-        
-        // Icons
+
+        // Fonts and Icons
         { rel: 'stylesheet', href: '/assets/vendor/fonts/boxicons.css' },
         { rel: 'stylesheet', href: '/assets/vendor/fonts/fontawesome.css' },
         { rel: 'stylesheet', href: '/assets/vendor/fonts/flag-icons.css' },
-  
+
         // Core CSS
         { rel: 'stylesheet', href: '/assets/vendor/css/rtl/core.css' },
         { rel: 'stylesheet', href: '/assets/vendor/css/rtl/theme-default.css' },
         { rel: 'stylesheet', href: '/assets/css/demo.css' },
         { rel: 'stylesheet', href: '/assets/vendor/css/rtl/rtl.css' },
-  
+
         // Vendors CSS
         { rel: 'stylesheet', href: '/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css' },
         { rel: 'stylesheet', href: '/assets/vendor/libs/typeahead-js/typeahead.css' },
-        { rel: 'stylesheet', href: '/assets/vendor/libs/apex-charts/apex-charts.css' },
+        { rel: 'stylesheet', href: '/assets/vendor/libs/apex-charts/apex-charts.css' }
       ],
       script: [
-        // Core JS
-        { src: '/assets/vendor/libs/jquery/jquery.js', type: 'text/javascript' },
-        { src: '/assets/vendor/libs/popper/popper.js', type: 'text/javascript' },
-        { src: '/assets/vendor/js/bootstrap.js', type: 'text/javascript' },
-        { src: '/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js', type: 'text/javascript' },
-        { src: '/assets/vendor/libs/hammer/hammer.js', type: 'text/javascript' },
-        { src: '/assets/vendor/libs/i18n/i18n.js', type: 'text/javascript' },
-        { src: '/assets/vendor/libs/typeahead-js/typeahead.js', type: 'text/javascript' },
-        { src: '/assets/vendor/js/menu.js', type: 'text/javascript' },
-  
-        // Vendors JS
-        { src: '/assets/vendor/libs/apex-charts/apexcharts.js', type: 'text/javascript' },
-  
-        // Main JS
-        { src: '/assets/js/main.js', type: 'text/javascript' },
-  
-        // Page JS
-        { src: '/assets/js/dashboards-analytics.js', type: 'text/javascript' },
-  
         // Helpers
-        { src: '/assets/vendor/js/helpers.js', type: 'text/javascript' },
-  
-        // Template customizer & Theme config files (include after core JS files)
-        { src: '/assets/js/config.js', type: 'text/javascript' }
+        { src: '/assets/vendor/js/helpers.js' },
+
+        // Template customizer & Config
+        { src: '/assets/vendor/js/template-customizer.js' },
+        { src: '/assets/js/config.js' }
       ]
     }
   },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }
 })
-// https://nuxt.com/docs/api/configuration/nuxt-config
-
-
---
+++ pages/login.vue
@@ -0,0 +1,193 @@
+<script setup>
+import { useHead } from '#imports'
+
+useHead({
+  title: 'ورود کاور - صفحات | فرست - قالب مدیریت بوت‌استرپ',
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0' },
+    { name: 'description', content: '' }
+  ],
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/assets/img/favicon/favicon.ico' },
+    { rel: 'stylesheet', href: '/assets/vendor/fonts/boxicons.css' },
+    { rel: 'stylesheet', href: '/assets/vendor/fonts/fontawesome.css' },
+    { rel: 'stylesheet', href: '/assets/vendor/fonts/flag-icons.css' },
+    { rel: 'stylesheet', href: '/assets/vendor/css/rtl/core.css' },
+    { rel: 'stylesheet', href: '/assets/vendor/css/rtl/theme-default.css' },
+    { rel: 'stylesheet', href: '/assets/css/demo.css' },
+    { rel: 'stylesheet', href: '/assets/vendor/css/rtl/rtl.css' },
+    { rel: 'stylesheet', href: '/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css' },
+    { rel: 'stylesheet', href: '/assets/vendor/libs/typeahead-js/typeahead.css' },
+    { rel: 'stylesheet', href: '/assets/vendor/libs/formvalidation/dist/css/formValidation.min.css' },
+    { rel: 'stylesheet', href: '/assets/vendor/css/pages/page-auth.css' }
+  ],
+  script: [
+    { src: '/assets/vendor/js/helpers.js', body: true },
+    { src: '/assets/vendor/js/template-customizer.js', body: true },
+    { src: '/assets/js/config.js', body: true },
+    { src: '/assets/vendor/libs/jquery/jquery.js', body: true },
+    { src: '/assets/vendor/libs/popper/popper.js', body: true },
+    { src: '/assets/vendor/js/bootstrap.js', body: true },
+    { src: '/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js', body: true },
+    { src: '/assets/vendor/libs/hammer/hammer.js', body: true },
+    { src: '/assets/vendor/libs/i18n/i18n.js', body: true },
+    { src: '/assets/vendor/libs/typeahead-js/typeahead.js', body: true },
+    { src: '/assets/vendor/js/menu.js', body: true },
+    { src: '/assets/vendor/libs/formvalidation/dist/js/FormValidation.min.js', body: true },
+    { src: '/assets/vendor/libs/formvalidation/dist/js/plugins/Bootstrap5.min.js', body: true },
+    { src: '/assets/vendor/libs/formvalidation/dist/js/plugins/AutoFocus.min.js', body: true },
+    { src: '/assets/js/main.js', body: true },
+    { src: '/assets/js/pages-auth.js', body: true }
+  ]
+})
+import { ref } from 'vue'
+
+const email = ref('programmer')
+const password = ref('Test@123')
+const remember = ref(false)
+const showPassword = ref(false)
+
+const togglePassword = () => {
+  showPassword.value = !showPassword.value
+}
+
+const handleLogin = async () => {
+  try {
+    // ارسال درخواست به API برای ورود
+    const res = await fetch('http://api.hicommerce.ir/api/Identity/Auth/Login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: email.value,
+        password: password.value,
+      }),
+    })
+
+    if (!res.ok) {
+      throw new Error('خطا در ورود به سیستم')
+    }
+
+    const data = await res.json()
+    console.log(data)
+    // در اینجا می‌توانید کارهایی مانند ذخیره‌سازی توکن، ریدایرکت و غیره انجام دهید
+  } catch (err) {
+    console.error('خطا در درخواست ورود:', err)
+  }
+}
+</script>
+
+<template>
+    <div class="authentication-wrapper authentication-cover">
+      <div class="authentication-inner row m-0">
+        <!-- /Left Text -->
+        <div class="d-none d-lg-flex col-lg-7 col-xl-8 align-items-center">
+          <div class="flex-row text-center mx-auto">
+            <img
+              src="/assets/img/pages/login-light.png"
+              alt="Auth Cover Bg color"
+              width="520"
+              class="img-fluid authentication-cover-img"
+              data-app-light-img="pages/login-light.png"
+              data-app-dark-img="pages/login-dark.png"
+            />
+            <div class="mx-auto">
+              <h3>به قالب مدیریت قدرتمند فرست خوش آمدید</h3>
+              <p>
+                کاملا مناسب برای هر سطح از توسعه‌دهندگان که به شما کمک می‌کند
+                <br />
+                تا پروژه و برنامه بزرگ بعدی خود را شروع کنید.
+              </p>
+            </div>
+          </div>
+        </div>
+        <!-- /Left Text -->
+  
+        <!-- Login -->
+        <div class="d-flex col-12 col-lg-5 col-xl-4 align-items-center authentication-bg p-sm-5 p-4">
+          <div class="w-px-400 mx-auto">
+            <!-- Logo -->
+            <div class="app-brand mb-4">
+              <a href="/" class="app-brand-link gap-2 mb-2">
+                <span class="app-brand-logo demo">
+                  <!-- آیکون SVG -->
+                  <!-- ... SVG حذف شده برای اختصار ... -->
+                </span>
+                <span class="app-brand-text demo h3 mb-0 fw-bold">فرست</span>
+              </a>
+            </div>
+            <!-- /Logo -->
+            <h4 class="mb-3 secondary-font">به فرست خوش آمدید!</h4>
+            <p class="mb-4">لطفا وارد حساب خود شده و ماجراجویی را شروع کنید</p>
+  
+            <form id="formAuthentication" class="mb-3" @submit.prevent="handleLogin">
+              <div class="mb-3">
+                <label for="email" class="form-label">ایمیل یا نام کاربری</label>
+                <input
+                  type="text"
+                  class="form-control text-start"
+                  id="email"
+                  v-model="email"
+                  placeholder="ایمیل یا نام کاربری خود را وارد کنید"
+                  dir="ltr"
+                />
+              </div>
+  
+              <div class="mb-3 form-password-toggle">
+                <div class="d-flex justify-content-between">
+                  <label class="form-label" for="password">رمز عبور</label>
+                  <NuxtLink to="/forgot-password"><small>رمز عبور را فراموش کردید؟</small></NuxtLink>
+                </div>
+                <div class="input-group input-group-merge">
+                  <input
+                    :type="showPassword ? 'text' : 'password'"
+                    id="password"
+                    class="form-control text-start"
+                    v-model="password"
+                    placeholder="············"
+                    dir="ltr"
+                  />
+                  <span class="input-group-text cursor-pointer" @click="togglePassword">
+                    <i :class="showPassword ? 'bx bx-show' : 'bx bx-hide'"></i>
+                  </span>
+                </div>
+              </div>
+  
+              <div class="mb-3">
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" id="remember-me" v-model="remember" />
+                  <label class="form-check-label" for="remember-me"> به خاطر سپاری </label>
+                </div>
+              </div>
+  
+              <button type="submit" class="btn btn-primary d-grid w-100">ورود</button>
+            </form>
+  
+            <p class="text-center">
+              <span>کاربر جدید هستید؟</span>
+              <NuxtLink to="/register"><span>یک حساب بسازید</span></NuxtLink>
+            </p>
+  
+            <div class="divider my-4">
+              <div class="divider-text">یا</div>
+            </div>
+  
+            <div class="d-flex justify-content-center">
+              <a href="#" class="btn btn-icon btn-label-facebook me-3">
+                <i class="tf-icons bx bxl-facebook"></i>
+              </a>
+              <a href="#" class="btn btn-icon btn-label-google-plus me-3">
+                <i class="tf-icons bx bxl-google-plus"></i>
+              </a>
+              <a href="#" class="btn btn-icon btn-label-twitter">
+                <i class="tf-icons bx bxl-twitter"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <!-- /Login -->
+      </div>
+    </div>
+  </template>
+  
